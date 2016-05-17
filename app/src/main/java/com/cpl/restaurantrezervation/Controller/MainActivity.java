0035f3a99a96191782f7537147efb916257cfbf7
Trinh Xuan Hung <trinh.hung@devacademy.ro>
2016-05-17 18:59:33 +0300

    first

---
+++ app/src/main/java/com/cpl/restaurantrezervation/Controller/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cpl.restaurantrezervation.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cpl.restaurantrezervation.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
